e5f873927fe3a453dd40de6abfa8d4a48ac210b9
aceysx <aceysx@163.com>
2019-12-14 17:34:03 +0800

    update config for gen jar & bootrun

--- src/main/java/com/ApplicationMain.java
+++ src/main/java/com/ApplicationMain.java
@@ -3,14 +3,18 @@ package com;
 import com.service.JavaService;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 
 public class ApplicationMain {
-    private static String projectDir = System.getProperty("user.dir") + "/src/resource/test.c";
+    private static String projectDir = "";
     private static JavaService javaService = new JavaService();
 
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("please enter code path to parse");
+        }
+        projectDir = args[0];
+
         javaService.parse(projectDir);
     }
 }
